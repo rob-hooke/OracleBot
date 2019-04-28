47c759c9630aa7d1995b7e699a0fd8bf261f6482
Boby Robert <bobyrobert3@gmail.com>
2019-04-28 22:32:06 +0530

    Initial commit

---
+++ OracleBot.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 28 19:57:43 2019
+
+@author: Admin123
+"""
+
---
+++ temp.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 28 20:27:36 2019
+
+@author: Admin1
