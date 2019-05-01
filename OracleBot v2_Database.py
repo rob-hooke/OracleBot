86f264b11764cb1697a935f0b9b0bfa64e3a6902
Boby Robert <bobyrobert3@gmail.com>
2019-05-02 00:28:45 +0530

    Chose relevent comments to be stored
    Chose comment only if score is > 2 which removes less important one .
    
    Also checks if current dcore id greater than existing comment score.
    
    Acceptable  data length constraint added along with deleted or removed.

--- OracleBot v2_Database.py
+++ OracleBot v2_Database.py
@@ -22,7 +22,18 @@ def create_table():
 def format(data):       # Function to replace Characters
     data = data.replace("\n"," newlinechar ").replace("\r"," newlinechar ").replace('"',"'")
     return data
-
+def find_existing_score(pid):
+    try:
+        sql = "select score from parent_reply where parent_id = '{}' limit 1".format(pid)
+        c.execute(sql)
+        result =  c.fetchone()
+        if result != None:
+            return result[0]
+        else:
+            return False
+    except Exception as e:
+        return False
+    
 def find_parent(pid):       #Function to store parent ID
     try:
         sql = "select comment from parent_reply where comment_id = '{}' limit 1".format(pid)
@@ -34,13 +45,23 @@ def find_parent(pid):       #Function to store parent ID
             return False
     except Exception as e:
         return False
+
+def acceptable(data):
+    if len(data.split(' ')) > 50 or len(data) < 1:
+        return False
+    elif len(data) > 1000:
+        return False
+    elif data == '[deleted]' or data == '[removed]':
+        return False
+    else:
+        return False
     
 if __name__ == "__main__":
     create_table()
     row_counter = 0     #Itertae rows no
     paired_rows = 0
     
-    with open("F:/.../{}/RC_{}".format(timeframe.split('-')[0], timeframe), buffer = 1000) as f:
+    with open("F:/.../{}/RC_{}".format(timeframe.split('-')[0], timeframe), buffering = 1000) as f:
         for row in f:
             row+counter += 1
             row = json.loads(row)
@@ -49,5 +70,10 @@ if __name__ == "__main__":
             created_utc = row['created_utc']
             score = row['score']
             subreddit = row['subreddit']
-            
             parent_data = find_parent(parent_id)
+            
+            if score >= 2:
+                existing_comment_score = find_existing_score(parent_id)
+                if existing_comment_score:
+                    if score > existing_comment_score:
+                        
