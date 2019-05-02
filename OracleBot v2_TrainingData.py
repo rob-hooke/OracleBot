1b87669d2026097bc4964e9e978cfc350a95080e
Boby Robert <bobyrobert3@gmail.com>
2019-05-02 23:03:09 +0530

    Create OracleBot v2_TrainingData.py
    Two files each (From & To) for Training and Testing model are created
    
    Each line in 'From' contains parent comment
    
    Corresponding line in 'To' contains reply comment
    
    Testing file contains 5000 lines (parent & reply commens)

---
+++ OracleBot v2_TrainingData.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  2 22:31:14 2019
+
+@author: Boby Robert
+"""
+
+import sqlite3
+import pandas as pd
+
+timeframes = ['2015-05']
+
+for timeframe in timeframes:
+    connection =  sqlite3.connect('{}.db'.format(timeframe))
+    c = connection.cursor()
+    limit = 5000    #No of data fethed from sql db
+    last_unix = 0   #help in buffer through database
+    cur_length = limit
+    counter = 0
+    test_done = False
+    while cur_length == limit:
+        df = pd.read_sql("select * from parent_reply where unix > {} and parent not null and score > 0 order by unix asc limit {}".format(last_unix, limit), connection)
+        last_unix = df.tail(1)['unix'].values[0]
+        cur_length = len(df)
+        if not test_done:           #initial 5000 for testng model
+            with open("test.from",'a', encoding = 'utf8') as f:
+                for content in df['parent'].values:
+                    f.write(content+'\n')
+            with open("test.to",'a', encoding = 'utf8') as f:
+                for content in df['comment'].values:
+                    f.write(content+'\n')
+
+            test_done = True        # Test dataset file done
+        else:                       # Subsequent 5000 are appended for training model
+            with open("train.from",'a', encoding = 'utf8') as f:
+                for content in df['parent'].values:
+                    f.write(content+'\n')
+            with open("train.to",'a', encoding = 'utf8') as f:
+                for content in df['comment'].values:
+                    f.write(content+'\n')
+        
+        counter += 1
+        if counter % 20 == 0:
+            print(counter*limit, 'rows completed')
+        
